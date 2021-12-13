10442b8568aecc4b1bd46850de468b0e88b40103
Bogruina <bogruina@gmail.com>
2021-12-13 11:17:38 +0700

    Добавлены тесты валидатора

---
+++ src/FlaskWurtz/FlaskWurtzUnitTests/ValidatorTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlaskWurthzSDK;
+using NUnit.Framework;
+
+namespace FlaskWurtzUnitTests
+{
+    [TestFixture]
+    class ValidatorTests
+    {
+        [TestCase(10,15,20,ParameterName.FlaskDiameter, 
+            TestName = "Value less than range")]
+        [TestCase(100,1,50, ParameterName.FlaskDiameter,
+            TestName = "Value over than range")]
+        public void TestValidator_InvalidValue(double invalidValue, 
+            double minValue, double maxValue, ParameterName parameter)
+        {
+
+            Assert.Throws<ArgumentException>(() => Validator.AssertRangeParameters(minValue,maxValue,
+                    invalidValue,parameter), $"value out of range");
+        }
+
+        [TestCase(10, 1, 20, ParameterName.FlaskDiameter,
+            TestName = "Value in the range")]
+        public void TestValidator_ValidValue(double invalidValue,
+            double minValue, double maxValue, ParameterName parameter)
+        {
+
+            Assert.DoesNotThrow(() => Validator.AssertRangeParameters(minValue, maxValue,
+                invalidValue, parameter), $"value out of range");
+        }
+    }
+}
