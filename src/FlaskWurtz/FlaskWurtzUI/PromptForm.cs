35a068e3d92fae159b1d4ff7b04505d3449d5dc1
Bogruina <bogruina@gmail.com>
2021-12-27 16:38:02 +0700

    Исправлены замечания по ПЗ

--- src/FlaskWurtz/FlaskWurtzUI/PromptForm.cs
+++ src/FlaskWurtz/FlaskWurtzUI/PromptForm.cs
@@ -4,16 +4,21 @@ using System.Windows.Forms;
 namespace FlaskWurthzUI
 {
     //TODO:
+    /// <summary>
+    /// Класс, который хранит форму
+    /// с изображением 3D модели колбы Вюрца.
+    ///<para> Cлужит подсказкой пользователю. </para>
+    /// </summary>
     public partial class PromptForm : Form
     {
+        /// <summary>
+        /// Инициализация формы с изображением
+        /// 3D модели колбы Вюрца
+        /// </summary>
         public PromptForm()
         {
             InitializeComponent();
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
